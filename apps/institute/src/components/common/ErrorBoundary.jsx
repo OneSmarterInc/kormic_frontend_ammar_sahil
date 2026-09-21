9f6e11e3b09446ed9dd98dbbe9afa2d760e7fc1d
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-21 20:05:12 +0530

    Use portal-core alias in apps/institute/src/components/common/ErrorBoundary.jsx

--- apps/institute/src/components/common/ErrorBoundary.jsx
+++ apps/institute/src/components/common/ErrorBoundary.jsx
@@ -1,1 +1,1 @@
-export { default } from "../../../../../packages/portal-core/src/components/common/ErrorBoundary.jsx";
+export { default } from "@kormic/portal-core/components/common/ErrorBoundary.jsx";
