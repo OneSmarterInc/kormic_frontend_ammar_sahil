95dca9ac0462486e6484a430df0f9bff93567521
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-23 20:16:36 +0530

    Use black button variant on institute dashboard

--- apps/institute/src/pages/institute/DashboardPage.jsx
+++ apps/institute/src/pages/institute/DashboardPage.jsx
@@ -101,7 +101,7 @@ export default function DashboardPage() {
               </div>
             </div>
             <Link to="/institute/upload">
-              <Button className="bg-black text-white hover:bg-black/90" icon={UploadCloud}>
+              <Button variant="black" icon={UploadCloud}>
                 Upload a list
               </Button>
             </Link>
